c2410416155e7b4347e25ba31c5b67ee6b06c781
lialittis <yu-tianchi@outlook.com>
2022-03-24 03:49:28 +0100

    update output path

--- 初赛/SDK/SDK_C++/CodeCraft-2022/src/CodeCraft-2022.cpp
+++ 初赛/SDK/SDK_C++/CodeCraft-2022/src/CodeCraft-2022.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <fstream>
 #include "helper.h"
+#include <bits/stdc++.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 int main() {
 
-  int M,N;
+  //int M,N;
   int QosLim = 0;
 
   // read csv data
@@ -27,18 +30,25 @@ int main() {
   else
     cerr<<"Could not open config file.\n";
 
-  M = tableDemand.names.size() - 1;
-  N = tableQos.contents.size();
+  cFile.close();
+
+  //M = tableDemand.names.size() - 1;
+  //N = tableQos.contents.size();
 
   // table to store the results
-  vector<vector<int>> results(M,vector<int>(N,0));
+  // vector<vector<int>> results(M,vector<int>(N,0));
 
   // output file
-  ofstream resFile("../output/solution.txt");
-
+  string path = "../output";
+  mkdir(path.c_str(),0777); // create directory
+  fstream resFile("../output/solution.txt",fstream::out | fstream::trunc);
+  if(!resFile.is_open())
+    cout<<"Problem of output"<<endl;
   // time
   for(int t = 0; t < int(tableDemand.contents.size()); t++) {
+    // temp
     for(int i = 1; i < int(tableDemand.names.size()); i++){
+      int tempresult = 0; // which is actually results[i-1][j]
       // assign the demand for client i at time t
       string name = tableDemand.names[i];
       // cout<<sizeof(name)<<endl;
@@ -47,10 +57,10 @@ int main() {
       for(int j = 0; j < int(tableQos.contents.size()); j++){
         if(stoi(tableQos.contents[j][i]) > QosLim) continue;
         // if there are enough space for client i
-        if(results[i-1][j] + demand < stoi(tableSiteBW.contents[j][1])){
-          results[i-1][j] += demand;
+        if(tempresult + demand < stoi(tableSiteBW.contents[j][1])){
+          tempresult += demand;
           //auto n_name = regex_replace(name,newlines_re,"");
-          resFile << name <<":<"<<tableSiteBW.contents[j][0]<<","<<demand<<">\n";
+          resFile << name <<":<"<<tableSiteBW.contents[j][0]<<","<<demand<<">\r\n";
           break;
         }
         else
@@ -58,5 +68,6 @@ int main() {
       }
     }
   }
+  resFile.close();
   return 0;
 }
