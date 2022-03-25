0ac69616aa59e4c1c0fe0b97160d4af32ee46e26
lialittis <yu-tianchi@outlook.com>
2022-03-26 00:05:15 +0100

    update

--- 初赛/SDK/SDK_C++/CodeCraft-2022/src/CodeCraft-2022.cpp
+++ 初赛/SDK/SDK_C++/CodeCraft-2022/src/CodeCraft-2022.cpp
@@ -33,38 +33,63 @@ int main() {
   cFile.close();
 
   //M = tableDemand.names.size() - 1;
-  //N = tableQos.contents.size();
+  int N = tableQos.contents.size();
 
   // table to store the results
   // vector<vector<int>> results(M,vector<int>(N,0));
 
   // output file
-  string path = "/output";
+  string path = "../output";
   mkdir(path.c_str(),0777); // create directory
-  fstream resFile("/output/solution.txt",fstream::out | fstream::trunc);
+  fstream resFile("../output/solution.txt",fstream::out | fstream::trunc);
   if(!resFile.is_open())
     cout<<"Problem of output"<<endl;
   // time
   for(int t = 0; t < int(tableDemand.contents.size()); t++) {
     // temp
+    vector<int> temp_usedBW(N,0);
     for(int i = 1; i < int(tableDemand.names.size()); i++){
-      int tempresult = 0; // which is actually results[i-1][j]
+      // int tempresult = 0; // which is actually results[i-1][j]
       // assign the demand for client i at time t
       string name = tableDemand.names[i];
       // cout<<sizeof(name)<<endl;
-      int demand = stoi(tableDemand.contents[t][i]);
+      int rest_demand = stoi(tableDemand.contents[t][i]);
+      int flag_firt = 1;
       // loop all edge nodes
       for(int j = 0; j < int(tableQos.contents.size()); j++){
-        if(stoi(tableQos.contents[j][i]) > QosLim) continue;
+        // edge name and BW in total
+        auto edge = tableSiteBW.contents[j][0];
+        int total_BW = stoi(tableSiteBW.contents[j][1]);
+        int rest_BW = total_BW - temp_usedBW[j];
+        // Firstly, remove all edge nodes which don't satisify requirement of qos
+        if(stoi(tableQos.contents[j][i]) > QosLim || rest_BW == 0) continue;
         // if there are enough space for client i
-        if(tempresult + demand < stoi(tableSiteBW.contents[j][1])){
-          tempresult += demand;
+        if( rest_demand <= rest_BW){
+          temp_usedBW[j] += rest_demand;
           //auto n_name = regex_replace(name,newlines_re,"");
-          resFile << name <<":<"<<tableSiteBW.contents[j][0]<<","<<demand<<">\r\n";
+          if(flag_firt) {
+            resFile << name <<":<"<<edge<<","<<rest_demand<<">\n";
+            flag_firt = 0;
+          }
+          else {
+            resFile<< ",<"<<edge<<","<<rest_demand<<">\n";
+          }
+          rest_demand = 0;
           break;
         }
-        else
-          continue; // we just use another new edge node
+        else{
+          int temp_demand =  rest_BW;
+          temp_usedBW[j] += temp_demand;
+          rest_demand -= temp_demand;
+          if(flag_firt) {
+            resFile << name <<":<"<<edge<<","<<temp_demand<<">";
+            flag_firt=0;
+          }
+          else {
+            resFile<< ",<"<<edge<<","<<temp_demand<<">";
+          }
+          // continue; // we just use another new edge node
+        }
       }
     }
   }
